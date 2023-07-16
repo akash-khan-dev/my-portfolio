9b2974e0d33fbae405dace76951b457d847b2788
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2023-07-16 14:30:58 +0600

    bootstrap navbar compnent implement

--- src/component/Layout/Layout.js
+++ src/component/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Menubar from "./navbar/Menubar";
 import Footer from "./footer/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Layout = ({ children }) => {
   return (
