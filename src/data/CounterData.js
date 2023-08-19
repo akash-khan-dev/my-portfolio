f60a5b013d99d7315ed12e5c950a3715b926a6ed
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2023-08-19 22:35:49 +0600

    counter up done

---
+++ src/data/CounterData.js
@@ -0,0 +1,19 @@
+export const CounterData = [
+  {
+    count: 30,
+    duration: 5,
+    discription: "Team Members",
+    pluse: "+",
+  },
+  {
+    count: 150,
+    duration: 5,
+    discription: "Completed Projects",
+  },
+  {
+    count: 50,
+    duration: 5,
+    discription: "Satisfied Clients",
+    pluse: "+",
+  },
+];
