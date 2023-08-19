ced9cb7b0d42da0e9b5169bf4cfd3d4f859b8ad8
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2023-08-19 22:36:24 +0600

    block data create

---
+++ src/data/BlogData.js
@@ -0,0 +1,30 @@
+import sliderOne from "../Images/slider_1.jpg";
+import sliderTwo from "../Images/slider_2.jpg";
+import sliderThree from "../Images/slider_3.jpg";
+const blogData = [
+  {
+    img: sliderOne,
+    date: "10 January 2023",
+    title: "Bitcoin and EconomicFreedom",
+    btn: "Read More",
+  },
+  {
+    img: sliderTwo,
+    date: "10 January 2023",
+    title: "Bitcoin and EconomicFreedom",
+    btn: "Read More",
+  },
+  {
+    img: sliderThree,
+    date: "10 January 2023",
+    title: "Bitcoin and EconomicFreedom",
+    btn: "Read More",
+  },
+  {
+    img: sliderOne,
+    date: "10 January 2023",
+    title: "Bitcoin and EconomicFreedom",
+    btn: "Read More",
+  },
+];
+export default blogData;
