1d50688f4e7a220cde7054b5b67d59e2aa696812
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2023-08-20 22:41:26 +0600

    add page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import About from "@/component/About";
+import Blog from "@/component/Blog";
 import Hero from "@/component/Hero";
 import Layout from "@/component/Layout/Layout";
+import Portfolio from "@/component/Portfolio";
 import Service from "@/component/Services";
 
 export default function Home() {
@@ -10,6 +12,8 @@ export default function Home() {
         <Hero />
         <About />
         <Service />
+        <Portfolio />
+        <Blog />
       </Layout>
     </>
   );
